f5842e5e34fc53e2238738810e1910795f7c980d
Robert Lee <rob@graphicdesign.ie>
2018-02-07 21:52:51 +0000

    Add team template
    Added template to display team metadata on the team post page.

---
+++ includes/process/team-template.php
@@ -0,0 +1,20 @@
+<div class="row">
+	<ul class="list-unstyled">
+	    <div class="col-md-3">
+		    <li><strong>LOCATION_I18N: </strong> LOCATION_PH</li>
+		    <li><strong>COUNTRY_I18N: </strong> COUNTRY_PH</li>
+	    </div>
+	    <div class="col-md-3">
+		    <li><strong>URL_I18N: </strong> URL_PH</li>
+		    <li><strong>EMAIL_I18N: </strong> EMAIL_PH</li>
+		    <li><strong>PHONE_I18N: </strong> PHONE_PH</li>
+	    </div>
+	    <div class="col-md-3">
+		    <li><strong>SPORT_I18N: </strong> SPORT_PH</li>
+		    <li>
+		        <strong>RATE_I18N: </strong>
+		        <div id="recipe_rating" class="rateit" data-rateit-resetable="false" data-rid="RECIPE_ID"></div>
+		    </li>
+	    </div>
+	</ul>
+</div>
